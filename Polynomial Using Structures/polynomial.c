bcba7bda919b14f473fbc2cc73a0acde54671586
Prathyush R Nair <prathyushrnair@gmail.com>
2025-07-20 22:41:37 +0530

    work in progres

---
+++ Polynomial Using Structures/polynomial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "polynomial.h"
+/*
+This program contains 3 parts
+polynomial.h-contains function definitions and data definitions
+polynomial.c-contains logic/code for all the functions
+main.c- what user will see
+
+these are the functions
+void insert_poly();
+void display();
+polynomial *add(polynomial *poly1,polynomial *poly2);
+polynomial *prdt(polynomial *poly1,polynomial *poly2);
+*/
