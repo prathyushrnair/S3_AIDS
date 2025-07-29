8b7088f86c33f8dc294ec794ea112b65e729b21b
Prathyush R Nair <prathyushrnair@gmail.com>
2025-07-29 05:46:30 +0530

    py all 11 exps completed

--- pythonProject1/Exp10.py
+++ pythonProject1/Exp10.py
@@ -10,8 +10,8 @@ add
 
 def insert(dictionary: dict, no: int):
     for i in range(no):
-        name_of_book = input("Enter the name of the book\n")
-        stock_of_book = int(input("Enter the current stock of the book\n"))
+        name_of_book = input("Enter the name of the book")
+        stock_of_book = int(input("Enter the current stock of the book"))
         dictionary[name_of_book] = stock_of_book
         # inserted the name as key and stock as value
 
@@ -19,11 +19,11 @@ def insert(dictionary: dict, no: int):
 def delete(dictionary: dict, book_name: str):
     for i, j in dictionary.items():
         if i == book_name:  # target found
-            stock1 = int(input("Enter the how much to remove\n"))
+            stock1 = int(input("Enter the how much to remove"))
             if stock1 < j:
                 dictionary[i] = j - stock1
             else:
-                print("Deletion not possible\n")
+                print("Deletion not possible")
 
 
 def update(dictionary: dict, book_name: str):
