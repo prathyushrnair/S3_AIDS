0efc55eb5519ea6637577cd7e2d82c8463bf0a34
Prathyush R Nair <116661504+prathyushrnair@users.noreply.github.com>
2025-08-28 09:23:12 +0530

    Delete pythonProject1 directory

--- pythonProject1/Python Projects/exp 14.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-height_midpoints = [137.5, 142.5, 147.5, 152.5]
-no_of_students = [4, 12, 16, 8]
-plt.bar(height_midpoints,no_of_students,width=5,color='blue',edgecolor='BLACK')
-plt.show()
